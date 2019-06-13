9b29a0e7bbb050088301e57d3798325e77252925
r <39985574+cartyrya@users.noreply.github.com>
2019-06-13 21:58:33 +0000

    Remove title from visualization

--- js/network.js
+++ js/network.js
@@ -32,9 +32,6 @@ d3.json("./data/dummy.json", function(error, graph) {
     .attr("x", 6)
     .attr("y", 3);
 
-  node.append("title")
-    .text(function(d) { return d.id; });
-
   simulation
     .nodes(graph.nodes)
     .on("tick", ticked);
