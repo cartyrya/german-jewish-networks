3f185e71d7de37c22143f0453335e3cfa05ed342
r <39985574+cartyrya@users.noreply.github.com>
2019-06-13 22:46:48 +0000

    Add webpage for network visualization with tooltip

---
+++ js/tooltip.js
@@ -0,0 +1,63 @@
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+d3.json("./data/dummy.json", function(error, graph) {
+
+  if (error) throw error;
+
+  var link = svg.append("g")
+      .attr("class", "links")
+      .style("stroke", "#aaa")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line");
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", 5);
+
+  var labels = node.append("text")
+    .text (function(d) {
+        return d.id;
+    })
+    .attr("x", 6)
+    .attr("y", 3);
+
+  node.append("title")
+    .text(function(d) { return d.id; });
+
+  simulation
+    .nodes(graph.nodes)
+    .on("tick", ticked);
+
+  simulation.force("link")
+    .links(graph.links);
+
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("r", 5)
+        .style("fill", "black")
+        .attr("cx", function (d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+});
